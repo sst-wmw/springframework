263a7a468e2edd5c68fa0f2a532bdfe964e822f6
richard <richard@wmw.com.br>
2014-06-11 13:26:30 -0300

    '

--- SpringFramework-1/src/main/java/br/com/wmw/sst/spring/MainAOP.java
+++ SpringFramework-1/src/main/java/br/com/wmw/sst/spring/MainAOP.java
@@ -1,12 +1,13 @@
+package br.com.wmw.sst.spring;
 import br.com.wmw.spring.domain.Cliente;
 import br.com.wmw.spring.domain.Produto;
 
 
-public class Main {
+public class MainAOP {
 
 	public static void main(String[] args) {
 
-		Cliente cliente = new Cliente(1, "Richard");
+		Cliente cliente = new Cliente(1, "Richard Elias Constantino");
 		System.out.println(cliente.toString());
 
 		System.out.println();
